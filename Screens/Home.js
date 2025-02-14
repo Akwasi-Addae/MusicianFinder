6b686041c11c2c7870c821235b3b12a4910bd237
Dennis Kwarteng <luispekerson@gmail.com>
2025-02-14 12:44:53 -0500

    Update Home.js

--- Screens/Home.js
+++ Screens/Home.js
@@ -7,6 +7,18 @@ import { StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-nativ
 const Home = ({route, navigation}) => {
   const [checked, setChecked] = useState(false);
   const { firstName, lastName, num, email } = route.params;
+  const toggleSwitch = () => {
+    setChecked(!checked);
+  };
+
+  const nav = () => {
+    console.log(firstName);
+    if (checked) {
+      navigation.navigate("MusicianLogin", {firstName: firstName, lastName: lastName, num: num, email: email})
+    } else {
+      navigation.navigate("Church")
+    }
+  };
 
   return (
     <View style={styles.container}>
@@ -24,9 +36,18 @@ const Home = ({route, navigation}) => {
           the app that connects churches and musicians in an efficient manner. 
         </Text>
 
+        {/* <Text> {JSON.stringify(firstName)} </Text> */}
         <Text style={styles.title}> If you are a musician, please turn the switch on. 
           Otherwise, leave it off</Text>
       </View>
+
+      <View>
+        <Switch value={checked}onValueChange={(value) => setChecked(value)}/>
+      </View>
+
+      <TouchableOpacity style={styles.loginButton} onPress={() => nav()}>
+        <Text style={styles.loginText}> Next </Text>
+      </TouchableOpacity>
     </View>
   );
 };
@@ -55,6 +76,38 @@ const styles = StyleSheet.create({
   inputContainer: {
     width: '80%', // Full width of the container for better alignment
     marginBottom: 20,
+  },
+  input: {
+    width: '100%', // Full width to make it responsive
+    height: 50,
+    borderColor: 'gray',
+    borderWidth: 1,
+    borderRadius: 8,
+    paddingHorizontal: 10,
+    backgroundColor: '#FFF',
+  },
+  loginButton: {
+    width: '80%', // Match input field width
+    backgroundColor: '#3B5998', // Facebook blue color
+    padding: 15,
+    borderRadius: 8,
+    alignItems: 'center',
+    marginTop: 20,
+  },
+  loginText: {
+    color: '#FFF',
+    fontWeight: 'bold',
+    fontSize: 18,
+  },
+  signUpText: {
+    color: '#3B5998',
+    fontWeight: 'bold',
+    marginTop: 10,
+  },
+  forgotPasswordText: {
+    color: '#3B5998',
+    marginTop: 5,
+    textDecorationLine: 'underline', // Underline for a link effect
   },
 });
 
