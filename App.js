f3f3883ee3a18188d2ada900bc4c0ea2ce060040
Dennis Kwarteng <luispekerson@gmail.com>
2025-04-12 23:26:30 -0400

    Update App.js

--- App.js
+++ App.js
@@ -3,14 +3,16 @@ import Chat from "./Screens/Chat";
 import Home from './Screens/Home';
 import Login from './Screens/Login';
 import Church from './Screens/Church';
-import ChurchInfo from "./Screens/ChurchInfo";
 import Signup from './Screens/Signup';
 import React, { useState } from 'react';
 import { StyleSheet} from 'react-native';
 import Settings from "./Screens/Settings";
 import SignupTwo from './Screens/SignupTwo';
 import HomeScreen from "./Screens/HomeScreen";
+import ChurchInfo from "./Screens/ChurchInfo";
 import MusicianLogin from './Screens/MusicianLogin';
+import CreateGigScreen from "./Screens/CreateGigScreen";
+import ChurchHomeScreen from "./Screens/ChurchHomeScreen";
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
@@ -29,6 +31,15 @@ function MusicianTabs() {
   );
 }
 
+function ChurchTabs() {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="ChurchHomeScreen" component={ChurchHomeScreen} options={{headerShown: false}}/>
+      <Tab.Screen name="CreateGigScreen" component={CreateGigScreen} options={{headerShown: false}}/>
+    </Tab.Navigator>
+  );
+}
+
 export default function App() { 
   return (
     <NavigationContainer>
@@ -42,6 +53,7 @@ export default function App() {
         {/* Use MusicianTabs instead of MusicianScreen */}
         <Stack.Screen name="MusicianTabs" component={MusicianTabs} options={{headerShown: false}}/>
         <Stack.Screen name="ChurchInfo" component={ChurchInfo} options={{headerShown: false}}/>
+        <Stack.Screen name="ChurchTabs" component={ChurchTabs} options={{headerShown: false}}/>
       </Stack.Navigator>
     </NavigationContainer>
   );
