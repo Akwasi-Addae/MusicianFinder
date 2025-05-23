a0f28ddc6ce2ebbb87cfbd4b15224e7c50001f12
Dennis Kwarteng <luispekerson@gmail.com>
2025-05-23 19:57:49 -0400

    Add files via upload

---
+++ Screens/UpdateGig.js
@@ -0,0 +1,18 @@
+import { Avatar } from '@rneui/themed';
+import React, { useState } from 'react';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, TextInput, TouchableOpacity, KeyboardAvoidingView, Platform, Alert } from 'react-native';
+
+const UpdateGig = ({route, navigation}) => {
+    return (
+        <View>
+            
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+
+});
+
+export default UpdateGig;
